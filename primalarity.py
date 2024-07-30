e55703620facc687be172c148e980bc6980f6902
SeelamGowthami <130341707+SeelamGowthami@users.noreply.github.com>
2024-07-30 21:24:42 +0530

    Add files via upload

---
+++ primalarity.py
@@ -0,0 +1,16 @@
+def solve():
+ n=int(input())
+ if n==1:
+    print("no")
+ if n==2:
+    print("yes")
+ i=2
+ while i*i<=n:
+    if (n%i==0):
+        print("no")
+ print("yes")
+
+t = 1
+t = int(input())
+for i in range(t):
+    solve()
