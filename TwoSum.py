e6bdc952d2bddc748f7c9cefe3a84480ddfa9a75
SeelamGowthami <130341707+SeelamGowthami@users.noreply.github.com>
2024-08-02 23:02:07 +0530

    Add files via upload

---
+++ TwoSum.py
@@ -0,0 +1,14 @@
+#Using Two Pointers
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            nums[i]=(nums[i],i)
+        nums.sort()
+        i,j=0,len(nums)-1
+        while(i<j):
+            if nums[i][0] + nums[j][0] >target:
+                j-=1
+            elif nums[i][0] + nums[j][0]<target:
+                i+=1
+            else:
+                return [nums[i][1],nums[j][1]]
+        return []
