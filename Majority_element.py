e4c660b88c49a01a18bf7f374546810244854c15
SeelamGowthami <130341707+SeelamGowthami@users.noreply.github.com>
2024-08-02 22:49:55 +0530

    Add files via upload

---
+++ Majority_element.py
@@ -0,0 +1,13 @@
+ def majorityElement(self, nums: List[int]) -> int:
+        c=0
+        m=None
+        for i in nums:
+            if c==0:
+                m=i
+                c=1
+            elif m==i:
+                c+=1
+            else:
+                c-=1
+        return m
+
